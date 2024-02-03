c1377cb660de3e02646f29f8ddf39cbf60620939
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-02-04 05:49:26 +0600

    shop page and product details

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -3,6 +3,9 @@ import Banner from "@/components/Banner";
 import { groq } from "next-sanity";
 import { client } from '../../../sanity/lib/client';
 import NewArrival from '@/components/NewArrival';
+import HomeBanner from '@/components/HomeBanner';
+import BestSellers from '@/components/BestSellers';
+import YearProduct from '@/components/YearProduct';
 
 
 export const revalidate = 10;
@@ -15,14 +18,28 @@ const newArrivalQuery = groq`*[_type == 'product' && position == 'New Arrivals']
   ...
   } | order(_createdAt asc)`;
 
+  const bestSellersQuery = groq`*[_type == 'product' && position == 'Bestsellers']{
+    ...
+   } | order(_createdAt asc)`;
+  const specialOffersQuery = groq`*[_type == 'product' && position == 'Special Offers']{
+    ...
+   } | order(_createdAt asc)`;
+
 
 const HomePage = async () => {
   const banners = await client.fetch(bannerQuery);
-  const newArrivalProducts = await client.fetch(newArrivalQuery);
+  const newArrivalProducts = await client.fetch(bestSellersQuery);
+  const bestSellersProducts = await client.fetch(newArrivalQuery);
+  const specialOffersProducts = await client.fetch(specialOffersQuery);
+
   return (
     <main className="text-sm overflow-hidden min-h-screen">
       <Banner banners={banners} />
       <NewArrival products={newArrivalProducts} />
+      <HomeBanner />
+      <BestSellers products={bestSellersProducts} title="Our Bestsellers" />
+      <YearProduct />
+      <BestSellers products={specialOffersProducts} title="Special Offers" />
     </main>
    
   )
