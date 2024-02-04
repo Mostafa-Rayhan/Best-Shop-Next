01e18700820c57f58e0e09b510f671bdbc6a3cf5
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-02-05 00:08:04 +0600

    cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-// import Image from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 // import logo from "@/assets/logoBlack.png";
 import { FaSearch } from "react-icons/fa";
