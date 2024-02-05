fa4f6952b6c574707d6ff3916bd6c3fe785fe2c1
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-02-05 06:23:05 +0600

    webhooks

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -2,5 +2,6 @@ import banner from "./banner";
 import blockContent from "./blockContent";
 import category from "./category";
 import product from "./product";
+import order from "./order";
 
-export const schemaTypes = [banner, product, category, blockContent];
+export const schemaTypes = [banner, product, category, blockContent, order];
